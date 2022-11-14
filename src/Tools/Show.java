fbf84bae695ae47366c77f2ca2c3ff2bd8e6a30f
xli <xinli@alu.edu.gva.es>
2022-11-14 19:01:44 +0100

    Primer commit

---
+++ src/Tools/Show.java
@@ -0,0 +1,12 @@
+package Tools;
+
+public class Show {
+    public static void show(String[][] matrix) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
